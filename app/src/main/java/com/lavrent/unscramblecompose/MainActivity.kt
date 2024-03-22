20eddb930e9f35833de3c35adefc072a84b3b82e
pavlav <pa.lavrentyev@gmail.com>
2024-03-22 15:07:12 +0300

    good version commit

---
+++ app/src/main/java/com/lavrent/unscramblecompose/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.lavrent.unscramblecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.lavrent.unscramblecompose.ui.GameScreen
+import com.lavrent.unscramblecompose.ui.theme.UnscrambleTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnscrambleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
